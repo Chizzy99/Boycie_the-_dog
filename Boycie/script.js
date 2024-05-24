67549325ac782e863da7e076a965ecf3007c201c
Chizzy99 <schesworth1@aol.com>
2024-05-24 13:30:35 +0100

    Code updated for readabilty. about page deletd and added to Home page. Footer added. JS, scroll added, play button still not active.

--- Boycie/script.js
+++ Boycie/script.js
@@ -1,21 +1,17 @@
 const button = document.querySelector('button');
 const video = document.querySelector('.play-wrapper');
 const close = document.querySelector('.vid-close');
-const play = document.querySelector('.play');
 
 button.addEventListener('click', () => {
     video.style.display = 'block';
+    video.scrollIntoView({behaviour: "smooth"});
 });
 
 close.addEventListener('click', () => {
     video.style.display = 'none';   
 });
 
-video.addEventListener('click',);
-function playPauseMedia() {
-    if (media.stopped) {
-        media.play();    
-    } else {
-        media.pause();
-    }
-}
+
+
+
+
